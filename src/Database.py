a6bdb85a42556e1b5d3a0b2129a91a45451cfd11
Steven Nichols <stevenanichols@bellsouth.net>
2010-11-19 19:12:06 -0500

    Minor tweaks

--- src/Database.py
+++ src/Database.py
@@ -33,10 +33,6 @@ class Database:
         row = self.cursor.fetchone()
         return row[0]
 
-        #cursor.execute("INSERT INTO `shortqut`.`times` (`road_id`, `time`,
-        #        `duration` ) VALUES ('4342791', CURRENT_TIMESTAMP ,
-        #        '00:23:41');")
-
     def getTimes(self):
         self.cursor.execute("SELECT * FROM `times`;")
         rows = self.cursor.fetchall()
@@ -44,14 +40,6 @@ class Database:
 
     def getNeighbor(self):
         self.cursor.execute("SELECT name, road_id, segment_id, int1lat, int1lon, int2lat, int2lon FROM road_names, (select road_id, segment_id, int1lat,int1lon,int2lat,int2lon from segments WHERE (int1lat = '28.5412667' and int1lon = '-81.1958727') or (int2lat = '28.5412667' and int2lon = '-81.1958727')) as a where a.road_id = road_names.id")
-        #self.cursor.execute("SELECT name, road_id, segment_id, int1lat," \
-        #        "int1lon, int2lat, int2lon FROM road_names," \
-        #        "(SELECT road_id, segment_id, int1lat, int1lon," \
-        #        "  int2lat, int2lon FROM `segments`" \
-        #        "  WHERE (int1lat = '28.5412667' AND int1lon = '-81.1958727')"\
-        #        "  OR (int2lat = '28.5412667' AND int2lon = '-81.1958727')" \
-        #        ") AS a" \
-        #        "WHERE a.road_id = road_names.id;")
         return self.cursor.fetchall()
 
 
