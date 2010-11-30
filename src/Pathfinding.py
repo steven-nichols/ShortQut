72e70320bc99d7f9fa50a24d9a9cd65af169f3b5
Steven Nichols <stevenanichols@bellsouth.net>
2010-11-29 20:56:50 -0500

    Minor fixes

--- src/Pathfinding.py
+++ src/Pathfinding.py
@@ -1,31 +1,15 @@
 #!/usr/bin/env python
+import cProfile
 import sys
 import os
 from PriorityQueue import PriorityQueue
-from Database import Database, name2cord
+from Database import Database
 #import Colorer
 from Log import Log
 log = Log('Pathfinding', 'info')
 
 
 #time_periods = 
-def writeResultsToFile(L):
-    '''A debug function'''
-    log.info("Writing partial results to file")
-    f = open("results.kml", "w")
-    f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-    f.write('<kml xmlns="http://www.opengis.net/kml/2.2">\n')
-    f.write('<Document>\n')
-    for node in L:
-        f.write('  <Placemark>\n')
-        f.write('   <name>%s</name>\n' % node)
-        f.write('   <Point>\n')
-        f.write('     <coordinates>%s,%s,0</coordinates>\n' % (name2cord(node)[1],name2cord(node)[0]))
-        f.write('   </Point>\n')
-        f.write(' </Placemark>\n')
-
-    f.write('</Document>\n')
-    f.write('</kml>\n')
 
 def weightedAvg2(mylist, current_time_period):
     '''Mylist is a list of tuples of form: (number, time period). The list
@@ -39,7 +23,7 @@ def weightedAvg2(mylist, current_time_period):
         number, period = entry[0], entry[1]
         if(period == current_time_period):
             curr.append(entry)
-    # Sort set by date
+    # Sort set by datel
     curr.sort()
     # Sort remaining entries by date
     mylist.sort()
@@ -103,10 +87,8 @@ class Pathfinding:
         Some examples::
         
             search = Pathfinding()
-            path = search.shortestPath("A", "C")
-            # returns ["A", "B", "C"]
-            cost = search.pathCost(path)
-            # returns 4
+            path = search.shortestPath("28.241378,-81.206989", "28.700301,-81.529274")
+            # returns ['28.6118857,-81.196140', '28.5798874,-81.1783859', '28.5799816,-81.175521']
         '''
         log.info("Start: {}, Goal: {}".format(start, goal))
         #path = self.aStarPath(start, goal, exceptions)
@@ -187,8 +169,8 @@ class Pathfinding:
                     tentative_is_better = False
 
                 if tentative_is_better == True:
-                    log.debug("Update node %s's weight to %g" % (y,
-															tentative_g_score))
+                    #log.debug("Update node %s's weight to %g" % (y,
+															#tentative_g_score))
                     came_from[y] = x
                     g_score[y] = tentative_g_score
                     h_score[y] = self.heuristicEstimateOfDistance(y, goal)
@@ -300,59 +282,60 @@ class Pathfinding:
         while len(forward) + len(backward) > 0:
             if len(forward) > 0:
                 done, stop = self.__dijkstraBiIter(start, goal, exceptions, 
-                                            dist_f, came_from_f, forward, 
-                                            closedset_forward, closedset_backward)
+                                        dist_f, came_from_f, forward, 
+                                        closedset_forward, closedset_backward)
 
             if not done and len(backward) > 0:
                 done, stop = self.__dijkstraBiIter(goal, start, exceptions,
-                                            dist_b, came_from_b, backward, 
-                                            closedset_backward, closedset_forward)
-            
+                                        dist_b, came_from_b, backward, 
+                                        closedset_backward, closedset_forward)
+        
             if done:
                 #log.debug("came_from_f: " + str(came_from_f))
                 #log.debug("came_from_b: " + str(came_from_b))
-                    
+                
                 pathf = self.reconstructPath(came_from_f, stop)
                 #log.info("PathF: %s" % pathf)
-                    
+                
                 pathb = self.reconstructPath(came_from_b, stop)
                 pathb.reverse()
                 #log.info("PathB: %s" % pathb)
+                
                 return pathf + pathb[1:]
-
         return None
-
+        
     def __dijkstraBiIter(self, start, goal, exceptions, dist, came_from, queue,
                             closedset, revclosedset):
         ''' Run a single iteration of dijkstra's algorithm. 
         returns:
             (bool, string) - tuple of form: (is search over?, vertex stopped on)
         '''
-        log.debug("queue: " + str(queue))
+        #log.debug("queue: " + str(queue))
         weight, x = queue.pop()
         dist[x] = weight
         if x == goal:
             return True, x
         elif x in revclosedset:
-            log.info("Meet in the middle: Node " + str(x))
+            #log.info("Meet in the middle: Node " + str(x))
             return True, x
-                
-        closedset.append(x)
             
+        closedset.append(x)
+        
         for y in self.neighborNodes(x):
             if y in closedset:
                 continue                
             if(exceptions is not None and y in exceptions):
                 continue
-                    
-            costxy = self.timeBetween(x,y)
                 
+
+            costxy = self.timeBetween(x,y)
+            
             if not dist.has_key(y) or dist[x] + costxy < dist[y]:
                 dist[y] = dist[x] + costxy
                 queue.reprioritize(dist[y], y)
                 came_from[y] = x
-                log.debug("Update node %s's weight to %g" % (y, dist[y]))
-                    
+                #log.debug("Update node %s's weight to %g" % (y, dist[y]))
+                
         return False, None
 
     def alternateRoute(self, num, optimal_path):
@@ -516,8 +499,8 @@ if __name__ == "__main__":
         
     #search = Pathfinding(True, graphfile)
     search = Pathfinding()
-
-    path = search.shortestPath("28.6118857,-81.196140", "28.5799816,-81.175521")
+    cProfile.run("search.shortestPath('28.241378,-81.206989', '28.700301,-81.529274')")
+    #path = search.shortestPath("28.241378,-81.206989", "28.700301,-81.529274")
     #alts = search.alternateRoute(3, path)
-    print path
-    writeResultsToFile(path)
+    #print path
+    #print search.pathCost(path)
