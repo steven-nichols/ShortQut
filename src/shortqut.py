61e5797c27d6cc016c247e3105aebb485c11ff41
Robby Ronk <robby.ronk@gmail.com>
2010-12-01 20:59:01 -0500

    I don't even know. Removed test-map.osm.

--- src/shortqut.py
+++ src/shortqut.py
@@ -20,17 +20,17 @@ def gpsfiletoroute(filename):
 class ShortqutGUI:
     
     def __init__(self):
-        global location
-        global talker
-        global loading
-        global track
-        global marker
-        global auto_center
-        talker = testtalker.TestTalker("data/gps2.out", 0)
-        talker.runLoop()
-        auto_center = True
-        loading = True
-        track = [] #where I've been
+        self.location = None #current (time, lat, lon)
+        self.loading = True #loading osm file?
+        self.track = [] #where I've been
+        self.track_route = None #route of track
+        self.marker = None #clutter marker of where I am right now
+        self.auto_center = True #auto center map
+        self.running = True
+        self.tick = 1
+
+        self.talker = testtalker.TestTalker("data/gps2.out", 0)
+        self.talker.runLoop()
 
         self.window = gtk.Window()
         self.window.set_border_width(10)
@@ -43,6 +43,7 @@ class ShortqutGUI:
         champ.set_size_request(640,480)
 
         self.view = champ.get_view()
+        help(self.view)
         self.view.set_reactive(True)
         self.view.connect('button-release-event', self.mouse_click_cb, self.view)
 
@@ -51,16 +52,13 @@ class ShortqutGUI:
         self.view.set_property('license-text', ".")
         self.view.set_property('show-scale', True)
         
-        layer = champlain.Layer()
-        self.view.add_layer(layer)
-        
         self.map_source_factory = champlain.map_source_factory_dup_default()
         self.map_data_source = None
         self.load_memphis_rules()
         self.load_osm_file()
         
-        #Add the image
         '''
+        #Add the image
         bboxTitle = gtk.HBox(False, 6)
         image = gtk.Image()
         image.set_from_file("Shortqut_banner.jpg")
@@ -72,6 +70,10 @@ class ShortqutGUI:
         #Add the buttons
         bbox = gtk.HBox(False, 6)
 
+        button = gtk.Button("Set")
+        button.connect("clicked", self.set_destination)
+        bbox.add(button)
+        
         #Add the label
         self.label = gtk.Label("Loading OSM File")
         bbox.add(self.label)
@@ -110,34 +112,50 @@ class ShortqutGUI:
         self.window.show_all()
 
 
-        #lat = 28.568542 + (random.random() - 0.5)
-        #lon = -81.207504 + (random.random() - 0.5)
+        '''
         lat = 28.541492
         lon = -81.195965
         self.view.center_on(lat,lon)
+        '''
         
+        self.marker = self.setup_marker()        
         
+        #self.draw_route(gpsfiletoroute("data/gps2.out"))
+
+        #self.location = self.talker.getMsg()
+        gobject.timeout_add(500, self.update_tick)
+        gobject.timeout_add(1000, self.is_loaded)
+
+    def set_destination(self):
+        pass
+
+    def setup_marker(self):
+        layer = champlain.Layer()
+        self.view.add_layer(layer)
         #Set up and position sprite
         actor = clutter.Texture(filename="pirate-ship-1.jpg")
         marker = champlain.marker_new_with_image(actor)
         marker.set_draw_background(False)
-        #marker.set_position(0,0)
-        marker.set_position(lat,lon)
+        marker.set_position(0,0)
         layer.add(marker)
         marker.raise_top()
-
-        self.draw_route(gpsfiletoroute("data/gps2.out"))
-
-        location = talker.getMsg()
-        #gobject.timeout_add(1000, random_view, self.view)
-        gobject.timeout_add(500, center_gps, self.view, location)
-        gobject.timeout_add(1000, is_loaded, self.view, self.label)
+        return marker
 
     def cleanup(self):
-        global talker
-        talker.setRunning(False)
+        self.talker.setRunning(False)
+        self.running = False
         gtk.main_quit()
         
+    def refresh_track_route(self):
+        self.tick += 1
+        if self.tick is not 7:
+            return
+        self.tick = 1
+        if len(self.track) > 1:
+            if self.track_route is not None:
+                self.view.remove_polygon(self.track_route)
+            self.track_route = self.draw_route(self.track, 'purple')
+            
     def draw_route(self, list_of_points, color='blue'):
         route = champlain.Polygon()
         for x in list_of_points:
@@ -145,11 +163,12 @@ class ShortqutGUI:
         route.set_stroke_width(5.0)
         route.set_stroke_color(clutter.color_from_string(color))
         self.view.add_polygon(route)
+        return route
         
     #If the box is checked, enable Automatic Centering
     def toggle_auto_centered(self, widget):
-        global auto_center
-        auto_center = False if auto_center else True
+        self.auto_center
+        self.auto_center = False if self.auto_center else True
     
     def zoom_in(self, widget):
         self.view.zoom_in()
@@ -161,7 +180,7 @@ class ShortqutGUI:
         lat, lon = view.get_coords_from_event(event)
         print "Mouse click at: %f %f" % (lat, lon)
         self.view.center_on(lat, lon)
-        return True
+        return self.running
 
     def zoom_changed(self, widget):
         self.view.set_property("zoom-level", self.spinbutton.get_value_as_int())
@@ -188,12 +207,10 @@ class ShortqutGUI:
         gobject.timeout_add(10, self._load_osm_file, osm_filename)
 		
     def _load_osm_file(self, filename):
-        global loading
         print "Loading osm file..."
         self.map_data_source.load_map_data(".%s%s" % (os.sep, filename))
         print "Done"
-
-        loading = False
+        self.loading = False
         
         self.source.set_map_data_source(self.map_data_source)
         return False
@@ -217,42 +234,26 @@ class ShortqutGUI:
         source_chain.push(self.source)
         source_chain.push(file_cache)
         self.view.set_map_source(source_chain)
+    
+    def update_tick(self):
+        if self.talker.messages.qsize() > 0:
+            self.location = self.talker.getMsg()
+            time, lat, lon = self.location
+            self.marker.set_position(lat,lon)
+        print "Go to: %f %f %f" % (time, lat, lon)
+        self.track.append( (lat,lon) )
+        self.refresh_track_route()
+        if self.auto_center:
+            self.view.center_on(lat, lon)
+        return self.running
 
-def random_view(view):
-    if(not (view.get_property("state") == champlain.STATE_LOADING)):
-        lat = 28.568542 + (random.random() - 0.5)
-        lon = -81.207504 + (random.random() - 0.5)
-        print "Go to: %f %f" % (lat, lon)
-        view.center_on(lat, lon)
-    return True
-
-def center_gps(view, gps_tuple):
-    global auto_center
-    global marker
-    global location
-    global talker
-    global track
-    if talker.messages.qsize() > 0:
-        location = talker.getMsg()
-        time, lat, lon = location
-        marker.set_position(lat,lon)
-    if len(track) > 0:
-        pass
-        
-    print "Go to: %f %f %f" % (time, lat, lon)
-    if auto_center:
-        view.center_on(lat, lon)
-    return True
-
-       
-def is_loaded(view, label):
-    global loading
-    if loading:
-        return True # have the timer run this fn again
-    else:
-        label.set_text("ShortQut")
-        view.zoom_in()
-        return False # the timer will fizzle out
+    def is_loaded(self):
+        if self.loading:
+            return True # have the timer run this fn again
+        else:
+            self.label.set_text("ShortQut")
+            self.view.zoom_in()
+            return False # the timer will fizzle out
 
 if __name__ == "__main__":
     gobject.threads_init()
